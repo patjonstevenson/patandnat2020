c7acc84ced952c75fe5bee1c5cddb99dbf779eba
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-01-06 22:21:27 -0800

    added image to home page

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import mainImage from "../images/engagement.jpeg";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="cover-photo">
-        <img src="#" />
+        <img src={mainImage} id="cover-image" />
       </div>
       <Header />
     </div>
