dbcf2965f7c26cff760af7651cc4e4dea98949ba
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-01-20 13:37:13 -0800

    got the gallery working! changed index.js for imports, curated the images

--- site/src/components/Gallery.js
+++ site/src/components/Gallery.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactGallery from "react-grid-gallery";
 
 // Pictures
-import images from "../images/gallery";
-import image1 from "../images/gallery/DSC05255.jpeg";
+import images from "../images/gallery/";
+// import image1 from "../images/gallery/DSC05255.jpeg";
 
 const Gallery = () => {
     console.log("Images:\n", images);
--- site/src/images/gallery/index.js
+++ site/src/images/gallery/index.js
@@ -1,197 +1,113 @@
 import fs from "fs";
 
-// import bigImages from "./";
-// import thumbs from "./thumbnails";
+// Big Images
+import image1 from "./big/DSC05306.jpeg";
+import image2 from "./big/DSC05308.jpeg";
+import image3 from "./big/DSC05330.jpeg";
+import image4 from "./big/DSC05341.jpeg";
+import image5 from "./big/DSC05346.jpeg";
+import image6 from "./big/DSC05360.jpeg";
+import image7 from "./big/DSC05364.jpeg";
+import image8 from "./big/DSC05367.jpeg";
+import image9 from "./big/DSC05371_edited.jpeg";
+import image10 from "./big/DSC05380.jpeg";
+import image11 from "./big/DSC05399.jpeg";
+import image12 from "./big/DSC05405.jpeg";
+import image13 from "./big/DSC05428.jpeg";
+import image14 from "./big/DSC05429.jpeg";
+import image15 from "./big/DSC05435.jpeg";
+import image16 from "./big/DSC05436.jpeg";
+import image17 from "./big/DSC05441.jpeg";
+import image18 from "./big/DSC05453.jpeg";
+import image19 from "./big/DSC05466_edited.jpeg";
+import image20 from "./big/DSC05471.jpeg";
+import image21 from "./big/DSC05475.jpeg";
+import image22 from "./big/DSC05476.jpeg";
+import image23 from "./big/DSC05483.jpeg";
+import image24 from "./big/DSC05485.jpeg";
 
-const imageSrc = [
-    "../images/gallery/DSC05255.jpeg",
-    "../images/gallery/DSC05258.jpeg",
-    "../images/gallery/DSC05262.jpeg",
-    "../images/gallery/DSC05264.jpeg",
-    "../images/gallery/DSC05265.jpeg",
-    "../images/gallery/DSC05273.jpeg",
-    "../images/gallery/DSC05275.jpeg",
-    "../images/gallery/DSC05289.jpeg",
-    "../images/gallery/DSC05291.jpeg",
-    "../images/gallery/DSC05293.jpeg",
-    "../images/gallery/DSC05294.jpeg",
-    "../images/gallery/DSC05297.jpeg",
-    "../images/gallery/DSC05299.jpeg",
-    "../images/gallery/DSC05302.jpeg",
-    "../images/gallery/DSC05303.jpeg",
-    "../images/gallery/DSC05305.jpeg",
-    "../images/gallery/DSC05306.jpeg",
-    "../images/gallery/DSC05308.jpeg",
-    "../images/gallery/DSC05312.jpeg",
-    "../images/gallery/DSC05323.jpeg",
-    "../images/gallery/DSC05328.jpeg",
-    "../images/gallery/DSC05330.jpeg",
-    "../images/gallery/DSC05336.jpeg",
-    "../images/gallery/DSC05341.jpeg",
-    "../images/gallery/DSC05342.jpeg",
-    "../images/gallery/DSC05345.jpeg",
-    "../images/gallery/DSC05346.jpeg",
-    "../images/gallery/DSC05349.jpeg",
-    "../images/gallery/DSC05351.jpeg",
-    "../images/gallery/DSC05353.jpeg",
-    "../images/gallery/DSC05354.jpeg",
-    "../images/gallery/DSC05356.jpeg",
-    "../images/gallery/DSC05360.jpeg",
-    "../images/gallery/DSC05362.jpeg",
-    "../images/gallery/DSC05363.jpeg",
-    "../images/gallery/DSC05364.jpeg",
-    "../images/gallery/DSC05366.jpeg",
-    "../images/gallery/DSC05367.jpeg",
-    "../images/gallery/DSC05369.jpeg",
-    "../images/gallery/DSC05370.jpeg",
-    "../images/gallery/DSC05371-edited.jpeg",
-    "../images/gallery/DSC05373-edited.jpeg",
-    "../images/gallery/DSC05374.jpeg",
-    "../images/gallery/DSC05379.jpeg",
-    "../images/gallery/DSC05380.jpeg",
-    "../images/gallery/DSC05382.jpeg",
-    "../images/gallery/DSC05399.jpeg",
-    "../images/gallery/DSC05401.jpeg",
-    "../images/gallery/DSC05405.jpeg",
-    "../images/gallery/DSC05422.jpeg",
-    "../images/gallery/DSC05428.jpeg",
-    "../images/gallery/DSC05429.jpeg",
-    "../images/gallery/DSC05435.jpeg",
-    "../images/gallery/DSC05436.jpeg",
-    "../images/gallery/DSC05437.jpeg",
-    "../images/gallery/DSC05439.jpeg",
-    "../images/gallery/DSC05440.jpeg",
-    "../images/gallery/DSC05441.jpeg",
-    "../images/gallery/DSC05442.jpeg",
-    "../images/gallery/DSC05447.jpeg",
-    "../images/gallery/DSC05450.jpeg",
-    "../images/gallery/DSC05451.jpeg",
-    "../images/gallery/DSC05452.jpeg",
-    "../images/gallery/DSC05453.jpeg",
-    "../images/gallery/DSC05455.jpeg",
-    "../images/gallery/DSC05456.jpeg",
-    "../images/gallery/DSC05458.jpeg",
-    "../images/gallery/DSC05460.jpeg",
-    "../images/gallery/DSC05461.jpeg",
-    "../images/gallery/DSC05462.jpeg",
-    "../images/gallery/DSC05463.jpeg",
-    "../images/gallery/DSC05465.jpeg",
-    "../images/gallery/DSC05466 -edited.jpeg",
-    "../images/gallery/DSC05466.jpeg",
-    "../images/gallery/DSC05469.jpeg",
-    "../images/gallery/DSC05470.jpeg",
-    "../images/gallery/DSC05471.jpeg",
-    "../images/gallery/DSC05475.jpeg",
-    "../images/gallery/DSC05476.jpeg",
-    "../images/gallery/DSC05477.jpeg",
-    "../images/gallery/DSC05478.jpeg",
-    "../images/gallery/DSC05479.jpeg",
-    "../images/gallery/DSC05480.jpeg",
-    "../images/gallery/DSC05482.jpeg",
-    "../images/gallery/DSC05483.jpeg",
-    "../images/gallery/DSC05485.jpeg",
-    "../images/gallery/DSC05486.jpeg",
-    "../images/gallery/DSC05487.jpeg",
-    "../images/gallery/DSC05488.jpeg",
-    "../images/gallery/DSC05492.jpeg",
-    "../images/gallery/FF3BD35B-A6F5-4203-BD77-52A440A3E2A0_1_105_c.jpeg"
+// Thumbnails
+import thumb_image1 from "./thumbnails/thumb_DSC05306.jpeg";
+import thumb_image2 from "./thumbnails/thumb_DSC05308.jpeg";
+import thumb_image3 from "./thumbnails/thumb_DSC05330.jpeg";
+import thumb_image4 from "./thumbnails/thumb_DSC05341.jpeg";
+import thumb_image5 from "./thumbnails/thumb_DSC05346.jpeg";
+import thumb_image6 from "./thumbnails/thumb_DSC05360.jpeg";
+import thumb_image7 from "./thumbnails/thumb_DSC05364.jpeg";
+import thumb_image8 from "./thumbnails/thumb_DSC05367.jpeg";
+import thumb_image9 from "./thumbnails/thumb_DSC05371_edited.jpeg";
+import thumb_image10 from "./thumbnails/thumb_DSC05380.jpeg";
+import thumb_image11 from "./thumbnails/thumb_DSC05399.jpeg";
+import thumb_image12 from "./thumbnails/thumb_DSC05405.jpeg";
+import thumb_image13 from "./thumbnails/thumb_DSC05428.jpeg";
+import thumb_image14 from "./thumbnails/thumb_DSC05429.jpeg";
+import thumb_image15 from "./thumbnails/thumb_DSC05435.jpeg";
+import thumb_image16 from "./thumbnails/thumb_DSC05436.jpeg";
+import thumb_image17 from "./thumbnails/thumb_DSC05441.jpeg";
+import thumb_image18 from "./thumbnails/thumb_DSC05453.jpeg";
+import thumb_image19 from "./thumbnails/thumb_DSC05466_edited.jpeg";
+import thumb_image20 from "./thumbnails/thumb_DSC05471.jpeg";
+import thumb_image21 from "./thumbnails/thumb_DSC05475.jpeg";
+import thumb_image22 from "./thumbnails/thumb_DSC05476.jpeg";
+import thumb_image23 from "./thumbnails/thumb_DSC05483.jpeg";
+import thumb_image24 from "./thumbnails/thumb_DSC05485.jpeg";
+
+const bigImages = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+    image21,
+    image22,
+    image23,
+    image24,
 ];
+
 
 const thumbnails = [
-    "../images/gallery/thumbnails/thumb-DSC05255.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05258.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05262.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05264.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05265.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05273.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05275.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05289.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05291.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05293.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05294.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05297.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05299.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05302.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05303.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05305.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05306.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05308.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05312.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05323.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05328.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05330.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05336.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05341.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05342.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05345.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05346.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05349.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05351.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05353.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05354.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05356.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05360.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05362.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05363.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05364.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05366.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05367.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05369.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05370.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05371-edited.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05373-edited.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05374.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05379.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05380.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05382.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05399.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05401.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05405.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05422.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05428.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05429.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05435.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05436.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05437.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05439.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05440.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05441.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05442.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05447.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05450.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05451.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05452.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05453.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05455.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05456.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05458.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05460.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05461.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05462.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05463.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05465.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05466 -edited.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05466.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05469.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05470.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05471.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05475.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05476.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05477.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05478.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05479.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05480.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05482.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05483.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05485.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05486.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05487.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05488.jpeg",
-    "../images/gallery/thumbnails/thumb-DSC05492.jpeg",
-    "../images/gallery/thumbnails/thumb-FF3BD35B-A6F5-4203-BD77-52A440A3E2A0_1_105_c.jpeg"
+    thumb_image1,
+    thumb_image2,
+    thumb_image3,
+    thumb_image4,
+    thumb_image5,
+    thumb_image6,
+    thumb_image7,
+    thumb_image8,
+    thumb_image9,
+    thumb_image10,
+    thumb_image11,
+    thumb_image12,
+    thumb_image13,
+    thumb_image14,
+    thumb_image15,
+    thumb_image16,
+    thumb_image17,
+    thumb_image18,
+    thumb_image19,
+    thumb_image20,
+    thumb_image21,
+    thumb_image22,
+    thumb_image23,
+    thumb_image24,
 ];
 
-const images = imageSrc.map((src, index) => (
+const images = bigImages.map((src, index) => (
     {
         src,
         thumbnail: thumbnails[index],
