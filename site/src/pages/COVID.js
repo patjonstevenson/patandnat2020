9bf5678e47d480d492678400b3a683f9e1a71086
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-06-22 14:23:25 -0700

    updated covid message

--- site/src/pages/COVID.js
+++ site/src/pages/COVID.js
@@ -5,7 +5,12 @@ export default () => {
         <div className="covid">
             <h1>COVID-19 Update</h1>
             <p>
-                Unfortunately, due to COVID-19, we have had to reduce the size of our wedding.
+                As much as we would love to celebrate our wedding with all of our friends and family 
+                present, in light of the current circumstances we will only be having our immediate 
+                family and wedding party in attendance.
+            </p>
+            <p>
+                We hope you will join us in thoughts and prayers on October 3rd.
             </p>
         </div>
     );
